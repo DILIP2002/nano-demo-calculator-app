917e1c695121311c67820d618d55cadc09f002b1
DILIP2002 <pushadapudileepnaidu@gmail.com>
2023-08-16 09:15:34 +0530

    ihi

--- server.py
+++ server.py
@@ -12,14 +12,16 @@ def add():
     data=request.json
     fn = data.get('firstNumber',0)
     sn = data.get('secondNumber',0) 
-    return jsonify({fn+sn}),200
+    r = fn+sn
+    return jsonify({'result':r}),200
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
     data = request.json
     fn = data.get('firstNumber',0)
     sn = data.get('secondNumber',0)
-    return jsonify({fn-sn}),200
+    r = fn-sn
+    return jsonify({'result':r}),200
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
